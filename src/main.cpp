9e60e971f3e7ced466709ac8c9596fa3ca516068
UNP <01unp01@gmail.com>
2025-10-12 09:52:39 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -13,29 +12,33 @@ int main() {
   std::string input;
   std::getline(std::cin, input);
   if(input == "exit 0"){
-    break;
+    return 0;
   }
-  std::stringstream parsed(input);
-  std::string word;
-  bool f=0;
-  while(parsed >> word){
-    if(f){
-      std::cout<<word<<" ";
-      continue;
-    }
-    if(word == "echo"){
-      f=1;
-      continue;
-    }
-    else{
-      break;
-    }
-  }
-  if(f){
-    std::cout<<std::endl;
-  }
+  if(input.find("echo") != std::string::npos) std::cout<<input.substr(5)<<std::endl;
   else{
     std::cout<< input <<": command not found"<< std::endl;
   }
+  // std::stringstream parsed(input);
+  // std::string word;
+  // bool f=0;
+  // while(parsed >> word){
+  //   if(f){
+  //     std::cout<<word<<" ";
+  //     continue;
+  //   }
+  //   if(word == "echo"){
+  //     f=1;
+  //     continue;
+  //   }
+  //   else{
+  //     break;
+  //   }
+  // }
+  // if(f){
+  //   std::cout<<std::endl;
+  // }
+  // else{
+  //   std::cout<< input <<": command not found"<< std::endl;
+  // }
   }
 }
