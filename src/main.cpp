962081fd8f8c26d859c3bd06f0f73b98a746a617
UNP <01unp01@gmail.com>
2025-10-12 10:01:11 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,19 @@ int main() {
   // Uncomment this block to pass the first stage
   while(true){
   std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
-  if(input == "exit 0"){
-    return 0;
-  }
-  if(input.substr(0,4) == "echo"){
-    std::cout<<input.substr(5)<<std::endl;
-  }
-  else{
-    std::cout<< input <<": command not found"<< std::endl;
-  }
+    std::string input;
+    std::getline(std::cin, input);
+
+    if (input == "exit 0") return 0;
+
+    if (input.rfind("echo", 0) == 0) { // starts with "echo"
+      if (input.size() > 5)
+        std::cout << input.substr(5) << std::endl;
+      else
+        std::cout << std::endl;
+    } else {
+      std::cout << input << ": command not found" << std::endl;
+    }
   // std::stringstream parsed(input);
   // std::string word;
   // bool f=0;
