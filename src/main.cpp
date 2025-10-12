2be75619b182d02c8b3990584f3e5b8ce5d75db4
UNP <01unp01@gmail.com>
2025-10-12 09:36:23 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@ int main() {
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
+  while(true){
   std::cout << "$ ";
-
   std::string input;
   std::getline(std::cin, input);
   std::cout<< input <<": command not found"<< std::endl;
+  }
 }
